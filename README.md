5ad70df789d687a39d81aa97fe3531681a2d1537
masonhaines <levi.haines97@gmail.com>
2025-07-11 12:41:07 -0700

    updates on express server.
    now allows cors cross origin requests for testing
    put token in testing phase

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 install nodeJS run - 
 
+
 npm install express
 npm init -y 
 npm i express
+npm install cors
 
 this installs express as well as creates the package.json file. npm i express includes the express dependency
--- express.js
+++ express.js
@@ -1,32 +1,36 @@
 // Mason Haines & Samuel Mouradian 7/9/2025
 
 const express = require('express');
+const fs = require('fs');
+const cors = require('cors'); // import CORS for cross-origin requests
 const path = require('path');
+
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // enable CORS for all routes
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
 
-// let token = {user: userName, browser: browserName};
+ // Request to server from client for some data 
+ // app.get("URL",(req,res)=>{})
+app.get("/token", (request, response) => {
+    var jData = fs.readFileSync('token.json'); // Read the token.json file
 
-// creates a JS object {user: name, browser: name} on Client
-function setToken(name) 
-{
+    console.log("GET Request Successfull!");
 
-}
+    response.send("request received for token.json");
+	
+})
 
-// writes a token from Client as a JSON file on the Server
-function putToken(token) 
-{
+app.post("/token", (request, response) => {
+    fs.writeFileSync('token.json', JSON.stringify(request.body, null, 2)); // Write the request body to token.json
 
-}
+	console.log(" TOKEN POST REQUEST SUCCESSFUL");
+	console.log(request.body);
 
-// reads the JSON file on the Server, returns a JS token to the Client
-function getToken()
- {
-
- }
+	response.send(`Data POST Request Recieved`);
+})
